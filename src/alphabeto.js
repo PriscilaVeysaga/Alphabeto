059f750ba7cee146514e94954f65e1b5cbc809b0
Priscila Veysaga <pri_veysaga@hotmail.com>
2023-04-16 13:42:41 -0300

    adicionado arquivo alphabeto

---
+++ src/alphabeto.js
@@ -0,0 +1,134 @@
+const alfabeto = [
+    {
+      minuscula: 'a',
+      maiuscula: 'A',
+      som: 'á',
+    },
+    {
+      minuscula: 'b',
+      maiuscula: 'B',
+      som: 'bê',
+    },
+    {
+      minuscula: 'c',
+      maiuscula: 'C',
+      som: 'cê',
+    },
+    {
+      minuscula: 'd',
+      maiuscula: 'D',
+      som: 'dê',
+    },
+    {
+      minuscula: 'e',
+      maiuscula: 'E',
+      som: 'é',
+    },
+    {
+      minuscula: 'f',
+      maiuscula: 'F',
+      som: 'éfê',
+    },
+    {
+      minuscula: 'g',
+      maiuscula: 'G',
+      som: 'gê',
+    },
+    {
+      minuscula: 'h',
+      maiuscula: 'H',
+      som: 'agá',
+    },
+    {
+      minuscula: 'i',
+      maiuscula: 'I',
+      som: 'i',
+    },
+    {
+      minuscula: 'j',
+      maiuscula: 'J',
+      som: 'jóta',
+    },
+    {
+      minuscula: 'k',
+      maiuscula: 'K',
+      som: 'cá',
+    },
+    {
+      minuscula: 'l',
+      maiuscula: 'L',
+      som: 'elê',
+    },
+    {
+      minuscula: 'm',
+      maiuscula: 'M',
+      som: 'emê',
+    },
+    {
+      minuscula: 'n',
+      maiuscula: 'N',
+      som: 'enê',
+    },
+    {
+      minuscula: 'o',
+      maiuscula: 'O',
+      som: 'ó',
+    },
+    {
+      minuscula: 'p',
+      maiuscula: 'P',
+      som: 'pê',
+    },
+    {
+      minuscula: 'q',
+      maiuscula: 'Q',
+      som: 'quê',
+    },
+    {
+      minuscula: 'r',
+      maiuscula: 'R',
+      som: 'érrê',
+    },
+    {
+      minuscula: 's',
+      maiuscula: 'S',
+      som: 'éssê',
+    },
+    {
+      minuscula: 't',
+      maiuscula: 'T',
+      som: 'tê',
+    },
+    {
+      minuscula: 'u',
+      maiuscula: 'U',
+      som: 'ú',
+    },
+    {
+      minuscula: 'v',
+      maiuscula: 'V',
+      som: 'vê',
+    },
+    {
+      minuscula: 'w',
+      maiuscula: 'W',
+      som: 'dáblio',
+    },
+    {
+      minuscula: 'x',
+      maiuscula: 'X',
+      som: 'xis',
+    },
+    {
+      minuscula: 'y',
+      maiuscula: 'Y',
+      som: 'ípsilon',
+    },
+    {
+      minuscula: 'z',
+      maiuscula: 'Z',
+      som: 'zê',
+    },
+  ];
+  
+  export default alfabeto;
